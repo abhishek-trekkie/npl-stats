84ab407dbe180ca76930e8917bbd2eae10472818
Habu Gabu <meabhi224@gmail.com>
2026-07-04 08:04:52 +0000

    ...

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/habu-gabu/npl-stats/internal/handler"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 )
@@ -12,10 +11,9 @@ func main() {
 
 	e.Use(middleware.RequestLogger())
 	e.Use(middleware.Recover())
+	e.Static("/static", "static")
 
-	e.GET("/", func(c *echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]string{"message": "Hello, World!"})
-	})
+	e.GET("/", handler.Home)
 
 	if err := e.Start(":1323"); err != nil {
 		e.Logger.Error("failed to start server", "error", err)
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.26.4
 
 require github.com/labstack/echo/v5 v5.2.1
 
-require golang.org/x/time v0.14.0 // indirect
+require (
+	github.com/a-h/templ v0.3.1020
+	golang.org/x/time v0.14.0 // indirect
+)
