9413d59608afa9a7e582f4de7d5774d5e84c8a24
abhishekshrestha <tech.abhishek@groupiig.com>
2026-07-06 15:07:53 +0545

    updated query

--- internal/sqlc/queries/players.sql
+++ internal/sqlc/queries/players.sql
@@ -1,13 +1,43 @@
 /* name: ListPlayers :many */
 SELECT
-    p.player_id,
-    p.player_name,
-    c.name AS country_name,
+    p.player_id as id,
+    p.player_name as name,
+
+    bat.name as batting_style,
+    ball.name as bowling_style,
+
+    c.name AS country,
     c.flag_code,
-    cc.name AS cricket_class
+
+    cc.name AS class
+
 FROM
-    players p
-    LEFT JOIN country c ON c.country_id = p.country_id
-    LEFT JOIN cricket_class cc ON cc.cricket_class_id = p.cricket_class_id
+  players p
+  LEFT JOIN batting_types bat ON bat.id = p.batting_type_id
+  LEFT JOIN bowling_types ball ON ball.id = p.bowling_type_id
+  LEFT JOIN cricket_class cc ON cc.id = p.cricket_class_id
+  LEFT JOIN country c ON c.id = p.country_id
 ORDER BY
     p.player_id;
+
+/* name: GetPlayerByID :one */
+SELECT
+    p.player_id as id,
+    p.player_name as name,
+
+    bat.name as batting_style,
+    ball.name as bowling_style,
+
+    c.name AS country,
+    c.flag_code,
+
+    cc.name AS class
+
+FROM
+  players p
+  LEFT JOIN batting_types bat ON bat.id = p.batting_type_id
+  LEFT JOIN bowling_types ball ON ball.id = p.bowling_type_id
+  LEFT JOIN cricket_class cc ON cc.id = p.cricket_class_id
+  LEFT JOIN country c ON c.id = p.country_id
+
+WHERE p.player_id = $1;
